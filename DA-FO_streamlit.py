4754e2e34627534bf6122c7860b5b02f09175601
HeiniB <heini.b@outlook.com>
2022-09-24 01:36:00 +0200

    Update DA-FO_streamlit.py

--- DA-FO_streamlit.py
+++ DA-FO_streamlit.py
@@ -15,7 +15,6 @@ ex = ""
 examples = ["Spiser du ikke til aften sammen med os?", "Jeg fik et nyt fotografiapparat"]
 
 df = pd.DataFrame([[55.676098, 12.568337], [62.007864, -6.790982]], columns = ["lat", "lon"])
-st.map(df)
                   
 st.title("Dansk-Færøsk Translator!")
 st.write("Eksempler på danske sætninger der kan oversættes")
@@ -25,3 +24,4 @@ st.markdown(ex)
 input_text = st.text_input("Dansk \U0001F1E9\U0001F1F0")
 output = translate_noatt(input_text)
 st.text_area(label = "Færøsk \U0001F1EB\U0001F1F4", value = output, height = 200, disabled = True)
+st.map(df)
