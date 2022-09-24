dcf993deb005128d7d2f696c58174cb5b61b4518
HeiniB <heini.b@outlook.com>
2022-09-24 13:25:41 +0200

    Update DA-FO_streamlit.py

--- DA-FO_streamlit.py
+++ DA-FO_streamlit.py
@@ -16,11 +16,11 @@ examples = ["Spiser du ikke til aften sammen med os?", "Jeg fik et nyt fotografi
 
 df = pd.DataFrame([[55.676098, 12.568337], [62.007864, -6.790982]], columns = ["lat", "lon"])
                   
-st.title("Dansk-Færøsk Translator!")
-st.write("Eksempler på danske sætninger der kan oversættes")
+st.title("Dansk-Færøsk Translator \U0001F1E9\U0001F1F0 \U0001F1EB\U0001F1F4")
+#st.write("Eksempler på danske sætninger der kan oversættes")
 for i in examples:
     ex += "- " + i + "\n"
-st.markdown(ex)
+st.markdown("Eksempler på danske sætninger der kan oversættes" + "\n" + ex)
 input_text = st.text_input("Dansk \U0001F1E9\U0001F1F0")
 output = translate_noatt(input_text)
 st.text_area(label = "Færøsk \U0001F1EB\U0001F1F4", value = output, height = 200, disabled = True)
